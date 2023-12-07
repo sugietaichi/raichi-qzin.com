fb403c0415da6bd81162991db37dbaac43f16c87
raichiqzin <raichiqzin@gmail.com>
2023-12-07 19:07:35 +0000

    dev

--- src/components/molecules/border-area/BorderArea.tsx
+++ src/components/molecules/border-area/BorderArea.tsx
@@ -6,8 +6,8 @@ export const BorderArea = ({
     children: React.ReactNode
 }) => {
     return (
-        <div className="border-gray-400 border rounded shadow-xl m-2 mb-5 p-2 pt-5 mt-10 relative">
-            {title && <div className="absolute top-[-1rem] left-2 px-2 bg-gray-200 border border-gray-400 text-xl">{title}</div>}
+        <div className="border-pink-700 border rounded shadow-xl m-2 mb-5 p-2 pt-5 mt-10 relative">
+            {title && <div className="absolute top-[-1rem] left-2 px-2 bg-pink-200 border border-pink-600 text-xl text-gray-700 "><p className="stroke-white">{title}</p></div>}
             {children}
         </div>
     )
